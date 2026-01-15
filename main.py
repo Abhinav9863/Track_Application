a192f0fe2828a75cd0547c292233f6fd20481dd0
Abhinav9863 <abhinavvishnumohan21@gmail.com>
2026-01-15 09:43:08 +0530

    Implement product retrieval, addition, update, and deletion endpoints

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ app = FastAPI()
 
 products = [
     Product(id = 1, name ="phone",description ="budget smartphone",price =99.0, quantity=10),
-    Product(id = 2, name ="laptop",description ="Budget Laptops",price =990.0, quantity=18)
+    Product(id = 2, name ="laptop",description ="Budget Laptops",price =990.0, quantity=18),
+    Product(id = 6, name ="laptop",description ="Budget Laptops",price =990.0, quantity=18)
+
 
 ]
 
@@ -19,4 +21,39 @@ def greet():
 def get_all_products():
     return products
 
+@app.get("/product/{id}")
+def get_product_by_id(id: int):
+    for product in products:
+        if product.id == id:
+            return product
+    
+    return "Product not found"
+
+
+@app.post("/product")
+def add_product(product:Product):
+    products.append(product)
+    return product
+
+@app.put("/product")
+def update_product(id :int ,product:Product):
+    for i in range(len(products)):
+        if products[i].id == id:
+            products[i] = product
+            return "product added successfully"
+    return "no Products found"
+
+@app.delete("/product")
+def delete_product(id: int ):
+    for i in range(len(products)):
+        if products[i].id == id:
+            del products[i]
+            return "product deleted successfully"
+    return "no Products found"  
+
+
+
+
+
+ 
 
