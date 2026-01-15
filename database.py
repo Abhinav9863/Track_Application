78167073b89ea8a5b25b728ff98ab1c1190c83fd
Abhinav9863 <abhinavvishnumohan21@gmail.com>
2026-01-15 17:24:18 +0530

    FINAL

--- database.py
+++ database.py
@@ -1,7 +1,14 @@
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
-db_url = "postgresql://abhinav:123456789@localhost:5432/sayone"
+db_url = os.getenv("DATABASE_URL")
+
+if db_url is None:
+    raise ValueError("DATABASE_URL environment variable is not set")
 engine = create_engine(db_url)
 session = sessionmaker(autocommit = False ,autoflush= False , bind=engine)
--- main.py
+++ main.py
@@ -3,8 +3,19 @@ from models import Product
 from database import session ,engine
 import database_models
 from sqlalchemy.orm import Session
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True,
+)
+
+
 
 database_models.Base.metadata.create_all(bind=engine)
 
@@ -48,7 +59,7 @@ def get_all_products(db :Session = Depends(get_db)):
 
     return db_products
 
-@app.get("/product/{id}")
+@app.get("/products/{id}")
 def get_product_by_id(id: int, db: Session = Depends(get_db)):
     db_product = db.query(database_models.Product).filter(database_models.Product.id == id).first()
     if db_product:
@@ -56,13 +67,13 @@ def get_product_by_id(id: int, db: Session = Depends(get_db)):
     return "Product not found"
 
 
-@app.post("/product")
+@app.post("/products")
 def add_product(product:Product , db: Session = Depends(get_db)):
     db.add(database_models.Product(**product.model_dump()))
     db.commit()
     return product
 
-@app.put("/product")
+@app.put("/products/{id}")
 def update_product(id :int ,product:Product,db: Session = Depends(get_db)):
     db_product = db.query(database_models.Product).filter(database_models.Product.id == id).first()
     if db_product:
@@ -75,13 +86,13 @@ def update_product(id :int ,product:Product,db: Session = Depends(get_db)):
     return "Product not found"
 
 
-@app.delete("/product")
-def delete_product(id: int ,Session = Depends(get_db)):
-    db_product = Session.query(database_models.Product).filter(database_models.Product.id == id).first()
+@app.delete("/products/{id}")
+def delete_product(id: int ,db : Session = Depends(get_db)):
+    db_product = db.query(database_models.Product).filter(database_models.Product.id == id).first()
     if db_product:
 
-        Session.delete(db_product)
-        Session.commit()
+        db.delete(db_product)
+        db.commit()
         return f"Product with id {id} has been deleted"
            
     return "no Products found"  
